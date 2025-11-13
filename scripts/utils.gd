fbb644186e5b7e86e7b749326700fdb9d8f4aacc
egeltorp <35803316+egeltorp@users.noreply.github.com>
2025-11-13 19:15:39 +0100

    WIP

--- scripts/utils.gd
+++ scripts/utils.gd
@@ -3,7 +3,7 @@ extends Node
 @onready var fps_label: Label
 
 func _ready() -> void:
-	fps_label = get_tree().get_current_scene().get_node("CanvasLayer/FPS")
+	fps_label = get_tree().get_current_scene().get_node("Debug/FPS")
 
 func _process(_delta: float) -> void:
 	fps_label.text = "FPS: %d" % Engine.get_frames_per_second()
